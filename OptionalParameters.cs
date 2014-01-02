221ffa78f9170be3bc61885811d03e5c7075ed66
Axel Prieto <prietoaxel@gmail.com>
2014-01-02 18:30:08 -0200

    Create OptionalParameters.cs

---
+++ OptionalParameters.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MasteringCSharp_BasicsOfSystem.Dynamic
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Foo("Rob");
+        //    Foo("Jon");
+        //    Foo("Eric");
+        //    Foo("hi", "man");
+        //}
+
+        //static void Foo(string x)
+        //{
+        //    Console.WriteLine("Hello " + x);
+        //}
+
+        //static void Foo(string x, string y)
+        //{
+        //    Console.WriteLine(x + " " + y);
+        //}
+
+        static void Main(string[] args)
+        {
+            Foo(y: "Rob");
+            Foo(y: "Jon");
+            Foo(y: "Eric");
+            Foo(x: "Ahoy", y: "sailor");
+            Foo("Hi", "Axel");
+            Foo();
+        }
+
+        static void Foo(string x = "Hello", string y = "dude")
+        {
+            Console.WriteLine(x + " " + y);
+        }
+
+        // Optional Parameters provide default values for arguments
+        // Instead of overloading methods, we can use Optional Parameters
+    }
+}
